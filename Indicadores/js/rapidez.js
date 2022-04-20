e44c99a194777c3af1638b5fc7914646fa6e49fc
tpmineriadatos <98907327+tpmineriadatos@users.noreply.github.com>
2022-04-20 18:38:46 -0500

    Proyecto Rapidez act Total tooltip

--- Indicadores/js/rapidez.js
+++ Indicadores/js/rapidez.js
@@ -1012,6 +1012,60 @@ function dibujaGraficaBarra(idGrafica, ejeX, datos2hrs, datos4hrs, datos6hrs) {
         },
         zoom: {
             enabled: true
+        },
+        tooltip: {
+            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+
+                function key_for_sum(arr) {
+                    return arr.value; //value is the key
+                }
+
+                function sum(prev, next) {
+                    return prev + next;
+                }
+
+                var totals_object = {};
+                totals_object.x = d[0]['x'];
+                totals_object.value = d.map(key_for_sum).reduce(sum); // sum func
+                totals_object.name = "Total a menos de 6 hrs"; //total will be shown in tooltip
+                totals_object.index = d[0]['index'];
+                totals_object.id = 'total'; //c3 will use this
+                d.push(totals_object);
+
+                var $$ = this,
+                    config = $$.config,
+                    titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                    nameFormat = config.tooltip_format_name || function (name) {
+                        return name;
+                    },
+                    valueFormat = config.tooltip_format_value || defaultValueFormat,
+                    text, i, title, value, name, bgcolor;
+
+                for (i = 0; i < d.length; i++) {
+
+                    if (!(d[i] && (d[i].value || d[i].value === 0))) {
+                        continue;
+                    }
+
+                    if (!text) {
+                        title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+                        text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+                    }
+
+                    name = nameFormat(d[i].name);
+                    value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+                    bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+                    text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+                    text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+                    text += "<td class='value'>" + value + "</td>";
+                    text += "</tr>";
+
+                }
+
+                return text + "</table>";
+
+            }
+
         }
 
     });
